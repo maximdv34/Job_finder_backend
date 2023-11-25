c44ddd42ed117dbec923bb0590e7d644630d216c
maximdv34 <maximdv34@gmail.com>
2023-11-25 23:29:21 +0100

    some changes

--- models/skillsModel.js
+++ models/skillsModel.js
@@ -4,18 +4,6 @@ const TechnicalSkillSchema = new mongoose.Schema({
     nome: { 
         type: String,
         required: [true, 'Un competenze tecniche deve avere un nome'],
-        /*
-        enum: {
-            values: [
-                'Disegno meccanico (Conoscenza e capacità di lettura)',
-                'Linguaggi di programmazione della produzione (conoscenza)', 
-                'Strumenti di misura (conoscenza e abilità)', 
-                'Progettazione (con strumenti CAD)',
-                'Controllare la qualità degli utensili prodotti (capacità)'
-                ],
-            message: ''
-        }
-        */
     },
 });
 
@@ -23,20 +11,6 @@ const SoftSkillSchema = new mongoose.Schema({
     nome: {
         type: String,
         required: [true, 'Un competenze trasversali deve avere un nome'],
-        /*
-        enum: {
-            values: [
-                'Competenze organizzative',
-                'Manualità e precisione', 
-                'Capacità di lavorare per obiettivi', 
-                'Capacità di lavorare in Team',
-                'Capacità di cooperazione e di coordinazione',
-                'Conoscenze informatiche ',
-                'Flessibilità e adattamento'
-                ],
-            message: ''
-        }
-        */
     }
 });
 
